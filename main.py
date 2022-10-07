ddfdcec1ed07579728bf528616b41b6d159072c3
dheerajroy <dhiraajroy123@gmail.com>
2022-10-07 18:35:51 +0530

    Added documentation to main.py

--- main.py
+++ main.py
@@ -7,14 +7,20 @@ blockchain = BlockChain()
 
 @app.get('/')
 def mine(data):
+    """Route to mine a block."""
+
     previous_block = blockchain.get_previous_block()
     proof = blockchain.proof_of_work(previous_block['proof'])
     return blockchain.create_block(proof, blockchain.hash(previous_block), data)
 
 @app.get('/chain')
 def get_chain():
+    """Route to view the chain."""
+
     return blockchain.chain
 
 @app.get('/is_chain_valid')
 def is_chain_valid():
+    """Route to validate the chain"""
+    
     return blockchain.is_chain_valid()
